3d6a454dcd49125bf9a6dec6f959d58ef1e761cd
Mxrti15 <mxrtiko2001@gmail.com>
2024-11-26 12:59:20 +0100

    Angular rutas

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core'; //Optimizacion
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
+import { routes } from './app.routes'; //importa desde app.routes.ts
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), //Mejora la detección de cambios
+    provideRouter(routes), // Configura rutas
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
+import {TasksComponents} from './pages/tasks/tasks.component';
+import {ProfileComponents} from './pages/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'', component: HomeComponent }, //pagina de inicio
+    { path:'tasks', component: TasksComponents },
+    { path:'profile', component: ProfileComponents },
+];
